d32e89cf2d05a156999b8adba2d86aad6f40dfb6
Harry Markley <tremarkley@gmail.com>
2018-03-29 15:57:23 -0700

    Added itemUrls object to keep track of urls for components

---
+++ server/itemUrls.js
@@ -0,0 +1,5 @@
+const Overview = process.env.OVERVIEW || 'http://localhost:3002'
+
+module.exports = {
+  Overview,
+}
--- server/server.js
+++ server/server.js
@@ -4,14 +4,11 @@ const path = require('path');
 const React = require('react');
 const ReactDom = require('react-dom/server');
 const axios = require('axios');
-// const styled = require('styled-components');
-// const morgan = require('morgan');
 const restaurantsInfoRouter = require('./routes/routes.js');
 const bundleRouter = require('./routes/bundleRouter.js');
 
 const app = express();
 const port = process.env.PORT || 4001;
-const overviewAPI = process.env.OVERVIEW || 'http://localhost:3002'
 
 app.use('/lib', express.static('public/lib'));
 app.use('/services', express.static(path.join(__dirname, './public/services')));
@@ -23,44 +20,18 @@ const services = require('./loader.js')(clientBundles, serverBundles, serviceCon
 const Layout = require('./templates/layout');
 const App = require('./templates/app');
 const Scripts = require('./templates/scripts');
+const APIUrls = require('./itemUrls');
 
 const renderComponents = async (components, props = {}) => {
   const results = [];
   for (item in components) {
-    const url = `${overviewAPI}/api/restaurants/${props.itemid}/${item}`;
+    const url = `${APIUrls[item]}/api/restaurants/${props.itemid}/${item}`;
     const response = await axios.get(url);
     let component = React.createElement(components[item], response.data);
     results.push({ string: ReactDom.renderToString(component), props: response.data });
   }
   return results;
-  // return Object.keys(components).map(async (item) => {
-  //   console.log(`item: ${item}`);
-  //   const url = `${overviewAPI}/api/restaurants/${props.itemid}/${item}`;
-  //   console.log(url);
-  //   const response = await axios.get(url);
-  //   let component = React.createElement(components[item], response.data);
-  //   return { string: ReactDom.renderToString(component), props: response.data };
-  //   // console.log('rendering components');
-  //   // res.write('<!DOCTYPE html><html><head><title>SSR Styled Components</title></head><body><div id="Overview">')
-  //   // const sheet = new styled.ServerStyleSheet()
-  //   // const jsx = sheet.collectStyles(React.createElement(components[item], props));
-  //   // console.log(jsx);
-  //   // const stream = sheet.interleaveWithNodeStream(ReactDom.renderToNodeStream(jsx));
-  //   // stream.pipe(res, { end: false });
-  //   // stream.on('end', () => res.end('</div></body></html>'));
-  // })
 }
-
-// // app.use(morgan('dev'));
-// app.get('/', (req, res) => {
-//   // res.redirect('/restaurants/ChIJUcXYWWGAhYARmjMY2bJAG2s/');
-//   res.redirect('/restaurants/1/');
-// })
-
-
-// app.get('/restaurants/:id/:widget/bundle.js', bundleRouter);
-
-// app.get('/api/restaurants/:id/:widget', restaurantsInfoRouter);
 
 app.get('/restaurants/:id', async function(req, res) {
   let components = await renderComponents(services, {itemid: req.params.id});
